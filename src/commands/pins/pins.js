1f833718743413411080fd129db7033298a47749
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-06-26 17:45:51 +0100

    Update pins.js

--- src/commands/pins/pins.js
+++ src/commands/pins/pins.js
@@ -5,12 +5,53 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('pins')
-        .setDescription('Explains how pins work on pc.')
-        .setDMPermission(false),
+        .setDescription('Explains how pins work.')
+        .setDMPermission(false)
+        .addSubcommand(subcommand => subcommand
+            .setName('pc')
+            .setDescription('Explains how pins work on pc.'),
+        )
+        .addSubcommand(subcommand => subcommand
+            .setName('mobile')
+            .setDescription('Explains how pins work on mobile/android.'),
+        )
+        .addSubcommand(subcommand => subcommand
+            .setName('iphone')
+            .setDescription('Explains how pins work on iphones.'),
+        ),
     async execute(interaction) {
-        await interaction.reply({
-            content: 'Please hoot the pins at the top right corner! https://imgur.com/a/0xiF2Nj',
-            ephemeral: true,
-        });
+        const subcommand = interaction.options.getSubcommand();
+
+        // Subcommand Handler
+        if (subcommand === 'pc') {
+            await handlePc(interaction);
+        }
+        else if (subcommand === 'mobile') {
+            await handleMobile(interaction);
+        }
+        else if (subcommand === 'iphone') {
+            await handleIphone(interaction);
+        }
     },
 };
+
+async function handlePc(interaction) {
+    await interaction.reply({
+        content: 'Please hoot the pins at the top right corner! https://imgur.com/a/0xiF2Nj',
+        ephemeral: true,
+    });
+}
+
+async function handleMobile(interaction) {
+    await interaction.reply({
+        content: 'Please hoot to the left to see the pins! https://cdn.discordapp.com/attachments/191561946892337153/723689034152411296/hoot_pins.png',
+        ephemeral: true,
+    });
+}
+
+async function handleIphone(interaction) {
+    await interaction.reply({
+        content: 'Please hoot to the left to see the pins! Also, buy a normal phone. https://cdn.discordapp.com/attachments/191561946892337153/723689034152411296/hoot_pins.png',
+        ephemeral: true,
+    });
+}
