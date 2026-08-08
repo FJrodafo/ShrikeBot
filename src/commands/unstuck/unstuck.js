bd29356f24556dffb781cd2482d9f16d7447ecc1
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-08 14:03:39 +0100

    Update unstuck.js [src/commands/unstuck]

--- src/commands/unstuck/unstuck.js
+++ src/commands/unstuck/unstuck.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('unstuck')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Instructs the user how to unstuck.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'Shrike points at the screen to show how to hoot yourself back to spawn/unstuck.\nhttps://cdn.discordapp.com/attachments/254109857819525132/725344496002531368/HelpImStuck.png',
+            ephemeral: false,
         });
     },
 };
