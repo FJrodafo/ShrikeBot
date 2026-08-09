5bf53161eeaa2bf604e06ac9b4a8681b4d77f665
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-09 11:59:39 +0100

    Update fashion.js [src/commands/links]

--- src/commands/links/fashion.js
+++ src/commands/links/fashion.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('fashion')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows images of all hunt pass armor sets on male and female models.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'Hoot\'s a link to a useful image with all the hunt pass set visuals!\nhttps://cdn.discordapp.com/attachments/555857529876381716/767773444649385984/All_hunt_pass_updated_version.png',
+            ephemeral: false,
         });
     },
 };
