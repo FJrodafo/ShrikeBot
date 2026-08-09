10fe126a854f4c8a98ae6c35dc5c7f206548cc7c
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-09 12:52:31 +0100

    Update meta-builds.js [src/commands/links]

--- src/commands/links/meta-builds.js
+++ src/commands/links/meta-builds.js
@@ -4,13 +4,13 @@ module.exports = {
     category: 'links',
     cooldown: 60,
     data: new SlashCommandBuilder()
-        .setName('metabuilds')
-        .setDescription('Working in progress... hooOOT!')
+        .setName('meta-builds')
+        .setDescription('Links to the meta spreadsheet.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: '<:hootrly:1333873657281511525> Hoot\'s a link to the Meta build spreadsheet, use it to see the top builds for the current patch! https://bit.ly/DauntlessMeta',
+            ephemeral: false,
         });
     },
 };
