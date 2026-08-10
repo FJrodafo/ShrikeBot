d4e7a0a55ecf9831fd21bd1ad605f3c2eb8c2bb7
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-10 12:30:45 +0100

    Update timeout.js [src/commands/info]

--- src/commands/info/timeout.js
+++ src/commands/info/timeout.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('timeout')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Provides a list of typical troubleshooting steps for experiencing server timeouts.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'If the servers keep hooting you out, consider these steps!\n1. Change your DNS to Google DNS: <https://developers.google.com/speed/public-dns>\n2. Reset your network adapter configuration: <https://kb.wisc.edu/dermatology/page.php?id=31480>\n3. Power your router off for 15 minutes, then turn it on again\n4. Use a VPN to try and get a better routing to game servers\n5. If none of the above works, consider hooting a request at official Support!',
+            ephemeral: false,
         });
     },
 };
