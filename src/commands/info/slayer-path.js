90b99592e56d41f062f482c62bcddb24b1874b64
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-10 13:58:39 +0100

    Update slayer-path.js [src/commands/info]

--- src/commands/info/slayer-path.js
+++ src/commands/info/slayer-path.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('slayer-path')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows a priority list for investing in your Slayer\'s Path.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: '__**Slayer Path upgrade priorities**__\n\n1. Mods and Specials\n2. Weapon upgrades\n3. Health, Stamina, Stamina regeneration\n4. Healing flask\n5. Tonics which you use *(recommended: Blitz, Frenzy, Aetherdrive)*\n6. Pylons which you use *(recommended: Inspiring)*\n7. Healing amplification\n8. Grenades you use *(recommended: Concussive)*\n9. Elemental resistance',
+            ephemeral: false,
         });
     },
 };
