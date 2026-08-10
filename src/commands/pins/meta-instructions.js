379cdd08e64be871ad1d437c2f374150ced73222
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-10 11:32:07 +0100

    Update meta-instructions.js [src/commands/pins]

--- src/commands/pins/meta-instructions.js
+++ src/commands/pins/meta-instructions.js
@@ -4,13 +4,13 @@ module.exports = {
     category: 'pins',
     cooldown: 60,
     data: new SlashCommandBuilder()
-        .setName('meta')
-        .setDescription('Working in progress... hooOOT!')
+        .setName('meta-instructions')
+        .setDescription('How to select good builds.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'Hoot\'s how to select good builds; https://cdn.discordapp.com/attachments/747985908317290606/747986244184834099/unknown-30.png',
+            ephemeral: false,
         });
     },
 };
