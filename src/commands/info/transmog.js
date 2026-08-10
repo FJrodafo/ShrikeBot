a5c5405a220cf39db260654373ba82615457cd31
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-10 12:27:06 +0100

    Update transmog.js [src/commands/info]

--- src/commands/info/transmog.js
+++ src/commands/info/transmog.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('transmog')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows how to apply transmogs.')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'Hoot\'s how to dress up!\nhttps://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOW93bTM5dTU5NDZ4ZG85YWUxeGRlenpybnpwdnJrZHdkZXcyOTF5aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3',
+            ephemeral: false,
         });
     },
 };
