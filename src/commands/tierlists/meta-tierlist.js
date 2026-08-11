da92ea2ef6e4867b95c09ec793db820a03ef12ac
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-11 13:57:36 +0100

    Update meta-tierlist.js [src/commands/tierlists]

--- src/commands/tierlists/meta-tierlist.js
+++ src/commands/tierlists/meta-tierlist.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('meta-tierlist')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows the meta tierlist. (up to debate)')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'Meta weapon tierlist (Based on Training Grounds Dummy DPS):\n1. Hammer\n2. Strikers\n3. Sword\n4. Chain Blades\n5. Axe, Pike, Repeaters (Based on behemoth)\n*This list is not a guide for the viability or overall ranking of each weapon, only potential dps taken from dummy dps. All weapons are generally viable for most content except when considering trials, and group trials strategies.*',
+            ephemeral: false,
         });
     },
 };
