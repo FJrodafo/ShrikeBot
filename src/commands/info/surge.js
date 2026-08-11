45e5aa7c0211760375788676ab4fa1466c0a9b75
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-11 12:14:32 +0100

    Update surge.js [src/commands/info]

--- src/commands/info/surge.js
+++ src/commands/info/surge.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('surge')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows advice on the priorities when Power Surging the gear!')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'When Power Surging your gear, remember this list!\n\n**Surge priority:**\n1. Your non-legendary weapon\n2. Your armour\n3. Your legendary weapon\n\nAlso remember that it is generally best to **surge the lowest tier gear first!** This will provide the greatest improvement to your build!',
+            ephemeral: false,
         });
     },
 };
