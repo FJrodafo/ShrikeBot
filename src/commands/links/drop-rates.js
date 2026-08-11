0ddc1f0d5d90465c6424f09b03c79665c921028e
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-11 13:40:11 +0100

    Update drop-rates.js [src/commands/links]

--- src/commands/links/drop-rates.js
+++ src/commands/links/drop-rates.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('drop-rates')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows a handy website with information about drop rates of various items in Dauntless')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: '<:hootrly:1333873657281511525> Here\'s a link: <https://scraplit.herokuapp.com/>! Shrike hoots you that it sometimes loads a bit slow. Give it some time, it\'ll get there!',
+            ephemeral: false,
         });
     },
 };
