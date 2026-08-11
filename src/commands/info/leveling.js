1f4510d2820dc58651751d8addab8b1d34a5a3b0
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-08-11 11:52:04 +0100

    Update leveling.js [src/commands/info]

--- src/commands/info/leveling.js
+++ src/commands/info/leveling.js
@@ -5,12 +5,12 @@ module.exports = {
     cooldown: 60,
     data: new SlashCommandBuilder()
         .setName('leveling')
-        .setDescription('Working in progress... hooOOT!')
+        .setDescription('Shows advice on quick weapon levelling!')
         .setDMPermission(false),
     async execute(interaction) {
         await interaction.reply({
-            content: 'Working in progress... hooOOT!',
-            ephemeral: true,
+            content: 'To hoot your weapon levels up fast, slay behemoths up to five levels higher than yours! **(These routes rely on using __Malkarion__ Weapons)**\n```\nSolo\n1-8: Fortune\'s Folly\n9-12: Undervald\n13-16: Razorcliff Isle\n17-20: The Blazeworks\n\nTeam\n1-8: Fortune\'s Folly\n9-14: Razorcliff Isle\n15-20: The Blazeworks```',
+            ephemeral: false,
         });
     },
 };
