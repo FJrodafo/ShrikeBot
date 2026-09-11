711e4c792ff52c4a317a6e738d87c30cf5a69347
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-09-11 20:55:54 +0100

    Update announcements.js [src/commands/pins] (Easter Egg)

--- src/commands/pins/announcements.js
+++ src/commands/pins/announcements.js
@@ -8,9 +8,18 @@ module.exports = {
         .setDescription('Tells users to check the annoucements channel ;P')
         .setDMPermission(false),
     async execute(interaction) {
+        const hoot = Math.random() < 1 / 100;
         await interaction.reply({
-            content: 'Shrike recommends you check out <#1333535056895016970>\nA good shrike checks it frequently! Hoot!',
+            content: 'Shrike recommends you check out <#1333535056895016970>\nA good shrike checks it frequently!',
             ephemeral: false,
         });
+        if (hoot) {
+            await interaction.channel.sendTyping();
+            await new Promise(resolve => setTimeout(resolve, 6000));
+            await interaction.channel.send({
+                content: 'those who don\'t want to be a good shrike can go fuck of',
+                ephemeral: false,
+            });
+        };
     },
 };
